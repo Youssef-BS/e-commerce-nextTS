4ca01f38987946897ca77189328f96ab94891f62
youssef-bs <youssefbensaid827@gmail.com>
2025-09-08 00:12:00 +0100

    complete category component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,18 @@
 "use client"
-import React from 'react'
+import { ProductType } from '@/types'
+import { Link } from 'lucide-react'
+import Image from 'next/image'
 
-const ProductCard = () => {
+const ProductCard = ({product} : {product:ProductType}) => {
   return (
-    <div>
+    <div className='shadow-lg rounded-lg overflow-hidden'>
+      <Link  href={`/product/${product.id}`}>
+      <div className='relative aspect-[2/3]'>
+        <Image src={product.images[product.colors[0]]} alt={product.name} fill className='object-cover hover:scale-105 transition-300'/>
+      </div>
+      </Link>
       
+
     </div>
   )
 }
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,7 @@
 import { ProductsType } from '@/types'
 import React from 'react'
+import Categories from './Categories'
+import ProductCard from './ProductCard'
 
 
 const products : ProductsType = [
@@ -116,7 +118,14 @@ const products : ProductsType = [
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <div className='w-full'>
+        <Categories />
+        <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12'>
+           {products.map(product => (
+              <ProductCard key={product.id} product={product}/>
+           ))}
+        </div>
+    </div>
   )
 }
 
