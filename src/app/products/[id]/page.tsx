bfc3acee9752a3a6e7577b21818e057eb60acad0
youssef-bs <youssefbensaid827@gmail.com>
2025-12-04 15:38:28 +0100

    new component added 'prodcutInteraction'

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ProductInteraction from "@/components/ProductInteraction";
 import { ProductType } from "@/types";
 import Image from "next/image";
 
@@ -43,6 +44,7 @@ const ProductPage = async ({
       <h1 className="text-2xl font-medium">{product.name}</h1>
       <p className="text-gray-500">{product.description}</p>
       <h2 className="text-2xl font-semibold">${product.price.toFixed(2)}</h2>
+      <ProductInteraction product={product} selectedSize={selectedSize} selectedColor={selectedColor}/>
       <div className="flex items-center gap-2 mt-4">
            <Image src="/Klarna.png" alt="Klarna" width={50} height={25} className="rounded-md"/>
             <Image src="/cards.png" alt="Cards" width={50} height={25} className="rounded-md"/>
