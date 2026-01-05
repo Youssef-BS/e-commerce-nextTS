5fe2e767de9ec2258ef3ed76d02337046249d283
youssef-bs <youssefbensaid827@gmail.com>
2026-01-05 19:54:34 +0100

    component ProfileButton added

--- micro service/apps/product-service/src/index.ts
+++ micro service/apps/product-service/src/index.ts
@@ -15,6 +15,27 @@ app.get('/health', (req : Request, res : Response) => {
   })
 })
 
+app.post('/api/products' , (req : Request , res : Response) => {
+    try{
+
+        const {name , price} = req.body ;
+        const productName = name ? name : "test" ; 
+        const productPrice = price ? price : 0 ;
+
+        return res.status(201).json({
+            id : Math.floor(Math.random() * 1000) , 
+            name : productName , 
+            price : productPrice
+        })
+
+    }catch(error){
+        return res.status(500).json({message : 'Internal serer error'})
+    } finally {
+        return res.status(201).json({message : 'Product created successfully'})
+    }
+
+})
+
 app.listen(8000 , ()=> {
     console.log('Product service is running on port 8000') ;
 })
